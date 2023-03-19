0cead48c63adebbd44bcf518335caced249aa268
ahmadsaleh-restaurant-menue <restaurant.menue.963@gmail.com>
2023-03-19 12:11:15 -0700

    first commit

---
+++ i.js
@@ -0,0 +1,42 @@
+'use strict' // 
+
+function chang() {
+
+    // console.log("sss")
+    // const para = document.querySelector('p')
+    // para.innerHTML = "AHMAD"
+    // console.log(Boolean(0))
+    // const quty = 200
+    // const price = 200
+    // const total = quty * price
+    // console.log(total)
+    // const reselt = total > 400 ? total = (total * 0.1) : total = (total * 0.15)
+    // console.log(reselt)
+
+
+    // const private = 2000
+    // console.log(private)
+
+
+
+    const arr = [200, 400, 500, 600, 300]
+    // const a = []
+    // arr.forEach(e => {
+    //     e = e + e * 0.15
+    //     a.push(e + e * 0.1)
+
+    // }
+
+    // )
+
+    // console.log(a)
+
+
+
+
+
+
+}
+
+
+
